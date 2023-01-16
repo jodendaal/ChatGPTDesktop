1f11eae76ad01a8f8ac34f0c1228a722e41719c5
Justim Odendaal <justim.odendaal@gmail.com>
2023-01-16 22:42:53 +0000

    Updated to be a single instance application

--- src/ChatGPTDesktop/ChatGPTDesktop/Program.cs
+++ src/ChatGPTDesktop/ChatGPTDesktop/Program.cs
@@ -1,17 +1,33 @@
+using NamedPipes.Helper;
+using System.Windows.Forms;
+
 namespace ChatGPTDesktop
 {
     internal static class Program
     {
+        static Form1 _form;
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+
+
+            App.SingleInstance(nameof(ChatGPTDesktop), () =>
+            {
+                _form = new Form1();
+                Application.Run(_form);
+            }, 
+            (message) => {
+                _form.ShowForm();
+            });
+
+           
         }
     }
 }
